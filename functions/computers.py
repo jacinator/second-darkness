6f5921399a0d700283bd77d804263333a381ca9f
Jace Petkau <jacepetkau@gfa.ca>
2019-08-14 17:07:44 -0400

    Refactoring the current ai.

--- functions/computers.py
+++ functions/computers.py
@@ -1,24 +1,21 @@
-import random
+from random import choices
 
-from templates import regions
+def _normalize_weights(weights):
+    diff = 1 - sorted(weights)[0]
+    return [w + diff for w in weights]
 
+def _computer_choose(choices, get_weight):
+    weights = [get_weight(c) for c in choices]
+    return choices(choices, _normalize_weights(weights))[0]
 
-def choose_neighbor(nation, region):
-    def _compare(neighbor):
-        return region.get_strength() / neighbor.get_strength()
-    neighbors = list(filter(
-        lambda x: x.occupants == nation,
-        region.get_neighbors(),
-    ))
-    weights = list(map(_compare, neighbors))
-    return random.choices(neighbors, weights)[0]
+def choose_friendly_neighbor(nation, region):
+    return _computer_choose(
+        list(region.get_friendly_neighbors()),
+        lambda n: sum(region.army.values()) / max(1, sum(n.army.values())),
+    )
 
-def choose_enemy(nation, region):
-    def _compare(neighbor):
-        return neighbor.get_strength() / region.get_strength()
-    neighbors = list(filter(
-        lambda x: x.occupants != nation,
-        region.get_neighbors(),
-    ))
-    weights = list(map(_compare, neighbors))
-    return random.choices(neighbors, weights)[0]
+def choose_hostile_neighbor(nation, region):
+    return _computer_choose(
+        list(region.get_hostile_neighbors()),
+        lambda n: sum(n.army.values()) / max(1, sum(region.army.values())),
+    )
