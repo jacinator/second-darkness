dcf3658ccf052920c3529750f41a39a3fd1ba3a5
Jace Petkau <jacepetkau@gfa.ca>
2019-08-15 11:19:18 -0400

    Expanding AI functions.

--- functions/computers.py
+++ functions/computers.py
@@ -1,4 +1,6 @@
-from random import choices
+from random import choices, random
+
+from functions.battles import battle
 
 def _normalize_weights(weights):
     diff = 1 - sorted(weights)[0]
@@ -19,3 +21,44 @@ def choose_hostile_neighbor(nation, region):
         list(region.get_hostile_neighbors()),
         lambda n: sum(n.army.values()) / max(1, sum(region.army.values())),
     )
+
+def choose_recruit_region(nation):
+    return _computer_choose(
+        [r for r in nation.get_regions() if r.developed],
+        lambda r: r.developed / sum(r.army.values()),
+    )
+
+def choose_recruitable_unit(nation, region):
+    return _computer_choose(
+        [u for u in nation.army if u.developed <= region.developed and u.cost <= nation.resources],
+        lambda u: (u.defense + u.offense) / u.cost,
+    )
+
+def choose_strong_region(nation):
+    return _computer_choose(
+        list(nation.get_regions()),
+        lambda r: sum(r.army.values()) / len(tuple(r.get_hostile_neighbors())),
+    )
+
+def run_computer(nation, turns=4):
+    fails = {"a": False, "m": False, "r": False}
+
+    while turns > 0 and not all(fails.values()):
+        if random() > 0.66:
+            region = choose_strong_region(nation)
+            against = choose_hostile_neighbor(nation, region)
+
+            battle(nation, region, against)
+
+        if random() > 0.66:
+            region = choose_strong_region(nation)
+            to = choose_friendly_neighbor(nation, region)
+
+            region.move_units(nation, to)
+
+        if random() > 0.66:
+            region = choose_recruit_region(nation)
+            unit = choose_recruitable_unit(nation, region)
+
+            nation.resources -= unit.cost
+            region.add_units({unit: 1})
